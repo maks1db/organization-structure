593b3cf265d861dd3499874e884c3b2879959e35
maksim.skvortsov <maksskvor@yandex.ru>
2022-02-27 17:15:52 +0300

    fix

--- src/backend/models/middlewares/lib.ts
+++ src/backend/models/middlewares/lib.ts
@@ -2,9 +2,6 @@ import { uniq } from 'ramda';
 import { ArtDbType } from './types';
 
 export const getArtDescription = (item: ArtDbType) => {
-  if (item.isRaw) {
-    return 'Требуется обновление арта';
-  }
   const employeesCount = uniq(
     item.employees.map(x => x.employee?.toString())
   ).length;
@@ -20,10 +17,6 @@ export const getTeamDescription = (item: ArtDbType) => {
   );
 
   return (teamId: string) => {
-    if (item.isRaw) {
-      return `Требуется обновление арта "${item.name}"`;
-    }
-
     return `Сотрудников: ${
       employeesTeams.filter(([, id]) => id === teamId).length
     }; Арт: ${item.name}`;
