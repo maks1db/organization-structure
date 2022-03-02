c2e1f71ff3e5ad0a82a5d9129c78d8817884229c
maksim.skvortsov <maksskvor@yandex.ru>
2022-03-02 15:20:33 +0300

    fix save

--- src/backend/controllers/entities/saveEntities.ts
+++ src/backend/controllers/entities/saveEntities.ts
@@ -12,16 +12,12 @@ export const saveArt: RequestHandler = async (req, res) => {
     const doc = await Art.findOne({ _id: typedBody._id });
     doc?.set({
       ...typedBody,
+      isRaw: false,
       positions: typedBody.positions.map(omitId),
       employees: typedBody.employees.map(omitId),
       teams: typedBody.teams.map(omitId),
     });
-    // await Art.updateOne({ _id: typedBody._id }, {
-    //   ...typedBody,
-    //   positions: typedBody.positions.map(omitId),
-    //   employees: typedBody.employees.map(omitId),
-    //   teams: typedBody.teams.map(omitId),
-    // } as ArtType);
+
     await doc?.save();
   } catch {
     res.sendStatus(500).send('Не удалось сохранить арт');
